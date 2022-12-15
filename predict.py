e4779ed4dad81c620ae8dee657d2cc45ed361ca1
Sam Plank <samuelplank@college.harvard.edu>
2022-12-15 14:30:21 -0500

    nit

--- predict.py
+++ predict.py
@@ -41,15 +41,10 @@ import subprocess
 from base64 import b64encode
 from cog import BasePredictor, Input, Path
 
-sys.path.append("./src/taming-transformers")
-sys.path.append("./src/clip")
-sys.path.append("./stable-diffusion/")
-sys.path.append("./k-diffusion")
-sys.path.append("..")
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print('This is dir_path')
-print(dir_path)
+sys.path.append("src/taming-transformers")
+sys.path.append("src/clip")
+sys.path.append("stable-diffusion/")
+sys.path.append("k-diffusion")
 
 from helpers import save_samples, sampler_fn
 from ldm.util import instantiate_from_config
